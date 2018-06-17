2dc944e7ecd534578e23547c730a91ced884ebe4
MargreetH <0margreet@gmail.com>
2018-06-17 17:34:21 +0200

    no message

--- AE2MainProgram.lua
+++ AE2MainProgram.lua
@@ -33,7 +33,7 @@ end
 --regetItems(pulverizerInterface)
 
 local fingerprint
-fingerprint = {id= "minecraft:gold_ingot", dmg = 0}
+fingerprint = {id= "ThermalFoundation:material", dmg = 0}
 print(fingerprint.id)
 print(fingerprint["id"])
 
