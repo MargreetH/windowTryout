d6e6af029f2ae2b15113764c0875f229a61928c5
MargreetH <0margreet@gmail.com>
2018-06-17 15:37:31 +0200

    no message

--- scripts/MEfunctions.lua
+++ scripts/MEfunctions.lua
@@ -1,10 +1,12 @@
 --Functions used by the ME program
 
 --Fills the whole chest with items
-function fillChest(interface)
-  local stacksTable
-  stacksTable = interface.getAllStacks(false)
-  for i = 1, #stacksTable do
-  print(stacksTable[i])
-end
+function fillChest(interface, side)
+  --local stacksTable
+  --stacksTable = interface.getAllStacks(false)
+
+  local canExportToSide
+  canExportToSide = interface.canExport("north")
+  print(canExportToSide)
+
 end
