2d8e68679068b5da4847db83b9ca4a8617caf0fa
MargreetH <0margreet@gmail.com>
2018-06-17 16:42:30 +0200

    no message

--- AE2MainProgram.lua
+++ AE2MainProgram.lua
@@ -27,9 +27,11 @@ function regetItems()
   numberOfItemTypesNetwork = #items
 end
 
+regetItems()
+
 fingerprint = {id= "ThermalFoundation:material", dmg = 0}
 local returnedHashes
-returnedHashes = MEfunctions.returnNBTHashes(fingerprint["id"])
+returnedHashes = MEfunctions.returnNBTHashes(fingerprint["id"], allItemsNetwork)
 
 for i = 1, #returnedHashes, 1 do
   fingerprint["nbt_hash"] = returnedHashes[i]
