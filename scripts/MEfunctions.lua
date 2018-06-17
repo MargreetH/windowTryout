35bedf6cd2a1d2f7e3d03ce0f215e1372195a00c
MargreetH <0margreet@gmail.com>
2018-06-17 15:10:10 +0200

    asadasd

--- scripts/MEfunctions.lua
+++ scripts/MEfunctions.lua
@@ -2,6 +2,7 @@
 
 --Fills the whole chest with items
 function fillChest(interface, chestLocation, fingerprint)
+  local stacksTable
   stacksTable = interface.getAllStacks(chestLocation)
   print(stacksTable[1])
 end
