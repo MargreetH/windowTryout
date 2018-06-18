8f768497fc4094e37de023ebf22f2091030c397e
MargreetH <0margreet@gmail.com>
2018-06-18 12:56:19 +0200

    no message

--- data/fingerprints.lua
+++ data/fingerprints.lua
@@ -5,3 +5,7 @@ pulverizedcopper = {id= "ThermalFoundation:material", dmg = 32}
 pulverizedlead = {id= "ThermalFoundation:material", dmg = 35}
 pulverizedsilver = {id= "ThermalFoundation:material", dmg = 34}
 pulverizedtin = {id= "ThermalFoundation:material", dmg = 33}
+printedsilicon = {id= "appliedenergistics2:item.Multimaterial", dmg = 20}
+printedlogiccircuit = {id= "appliedenergistics2:item.Multimaterial", dmg = 18}
+printedengineeringcircuit = {id= "appliedenergistics2:item.Multimaterial", dmg = 17}
+silicon = {id= "EnderIO:itemMaterial", dmg = 0}
