4dd0ebd312250398b6996b4d703f8e13a65af212
MargreetH <0margreet@gmail.com>
2018-06-20 02:23:48 +0200

    no message

--- snake.lua
+++ snake.lua
@@ -1,3 +1,24 @@
+local currentMovingDirection = {1, 0}
+local currentPosition = {20, 7}
+local snakeBlockCoordinates = {}
+snakeBlockCoordinates[1] = currentPosition
+snakeBlockCoordinates[2] = {19, 7}
+snakeBlockCoordinates[3] = {18, 7}
+local snakeBlockWindows = {}
+
+
+function createSnakeBlock(snakeblockcoordinates)
+  index = #snakeBlockCoordinates + 1
+  snakeBlockWindows[index] = window.create(term, snakeBlockCoordinates[1], snakeBlockCoordinates[2], 1, 1)
+  snakeBlockWindows[index].setBackgroundColor(1)
+  snakeBlockWindows[index].clear()
+end
+
+--Create first few blocks
+createSnakeBlock(snakeBlockCoordinates[1])
+createSnakeBlock(snakeBlockCoordinates[2])
+createSnakeBlock(snakeBlockCoordinates[3])
+
 function touchEvent(xPos, yPos)
 
 end
@@ -26,10 +47,16 @@ end
 
 print("plap")
 
+function nextStep()
+  term.clear()
+
+
+end
+
 
 local mainBoolean = true
 
 while mainBoolean do
-print(term.getSize())
-wait(0.2)
+
+  wait(0.2)
 end
