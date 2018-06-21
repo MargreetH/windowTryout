4b0a9f53b88790efe6b8e0285bd531f81a665560
MargreetH <0margreet@gmail.com>
2018-06-21 13:30:13 +0200

    no message

--- turtle/spawnWithers.lua
+++ turtle/spawnWithers.lua
@@ -7,10 +7,21 @@ print("plap")
 local soulsandSlots = {}
 local witherSlots = {}
 local allSlotDetails = {}
-
+local detail
 
 for i = 1, 16, 1 do
-  allSlotDetails[i] = turtle.getItemDetail(i)
+  detail = turtle.getItemDetail(i)
+  if (detail.id == fingerprints.soulsand.id)  then
+    print("foundone")
+    soulsandSlots[countersoulsand] = detail
+    countersoulsand = countersoulsand + 1
+    soulsandSlots[countersoulsand].index = i
+  end
+  if (detail.id == fingerprints.witherskull.id)  then
+    witherSlots[counterwitherslots] = detail
+    counterwitherslots = counterwitherslots + 1
+    witherSlots[counterwitherslots].index = i
+  end
 end
 
 local countersoulsand = 1
@@ -25,18 +36,11 @@ end
 
 --Check where the soulsand and wither skulls are
 for i = 1, #allSlotDetails, 1 do
-  if (allSlotDetails[i].id == fingerprints.soulsand.id)  then
-    print("foundone")
-    soulsandSlots[countersoulsand] = i
-    countersoulsand = countersoulsand + 1
-  end
-  if (allSlotDetails[i].id == fingerprints.witherskull.id)  then
-    witherSlots[counterwitherslots] = i
-    counterwitherslots = counterwitherslots + 1
-  end
+
+
 end
 
-functions.printTableToTerminal(soulsandSlots)
+functions.printTableToTerminal(soulsandSlots[1])
 
 
 function placeBlockBehind()
