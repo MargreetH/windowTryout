0c3edc87e1cae62de1b767dee04149585304a674
MargreetH <0margreet@gmail.com>
2018-06-21 13:03:56 +0200

    no message

--- turtle/spawnWithers.lua
+++ turtle/spawnWithers.lua
@@ -3,3 +3,18 @@ os.loadAPI('/git/data/fingerprints')
 
 
 print("plap")
+
+local soulsandSlots = {}
+local witherSlots = {}
+
+local allSlotDetails = {}
+local allSlotAmounts = {}
+
+for i = 1, 16, 1 do
+  allSlotDetails[i] = turtle.getItemDetail(i)
+  allSlotAmounts[i] = turtle.getItemCount(i)
+end
+
+functions.printTableToTerminal(allSlotDetails[1])
+functions.printTableToTerminal(allSlotDetails[2])
+functions.printTableToTerminal(allSlotDetails[3])
