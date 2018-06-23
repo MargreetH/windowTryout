569c427b98fc0464b24b66e39830356213de321e
MargreetH <0margreet@gmail.com>
2018-06-24 01:02:15 +0200

    no message

--- AE2MainProgramV2.lua
+++ AE2MainProgramV2.lua
@@ -363,23 +363,8 @@ end
 
 
 
-function createTopPart(w)
-  w.setBackgroundColor(16384) --Red
-  w.setTextColor(32768) --Black
-  w.write("****************************************************************")
-  functions.newLine(w)
-  w.write("***********  AE2 functions program  ********************************")
-  functions.newLine(w)
-  w.write("****************************************************************")
-  functions.newLine(w)
-  w.setTextColor(64) --Pink
-  w.setBackgroundColor(32768) --Black
-  w.write("Written by Merlione404")
-end
-
-
 --REPLACE THIS ALL BY IMPLEMENTING .styleWindow per component
-createTopPart(topPart)
+V2Layout.createTopPart(topPart)
 createInfoFieldStartWindow()
 createWindowSwitchFurnace()
 createWindowSwitchPulverizer()
--- V2Layout.lua
+++ V2Layout.lua
@@ -1,1 +1,13 @@
-os.loadAPI("/git/scripts/functions")
+function createTopPart(w)
+  w.setBackgroundColor(16384) --Red
+  w.setTextColor(32768) --Black
+  w.write("****************************************************************")
+  functions.newLine(w)
+  w.write("***********  AE2 functions program  ********************************")
+  functions.newLine(w)
+  w.write("****************************************************************")
+  functions.newLine(w)
+  w.setTextColor(64) --Pink
+  w.setBackgroundColor(32768) --Black
+  w.write("Written by Merlione404")
+end
